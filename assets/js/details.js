d18ee12284917afd890915405704f1fd441f5e71
MedaminSatou <aminemohamedsatou@gmail.com>
2024-11-18 15:15:55 +0100

    fixe problem details

--- assets/js/details.js
+++ assets/js/details.js
@@ -17,28 +17,24 @@ document.addEventListener("DOMContentLoaded", () => {
         return;
       }
 
-
       document.querySelector(".title").textContent = bookTitle;
       document.querySelector(".description").textContent = book.description;
       document.querySelector(".img").src = book.image;
-      
 
       const genreContainer = document.querySelector(".tags");
       const genreSpans = genreContainer.querySelectorAll(".genre");
-      
 
       genreSpans.forEach((span, index) => {
         if (index < book.genres.length) {
-          span.textContent = book.genres[index] + (index < book.genres.length - 1 ? ',' : '');
+          span.textContent =
+            book.genres[index] + (index < book.genres.length - 1 ? "," : "");
         } else {
-          span.textContent = '';
+          span.textContent = "";
         }
       });
 
-
       const yearSpan = document.querySelector(".year");
       yearSpan.textContent = book.year;
-
 
       document.querySelector(".read-btn").onclick = () =>
         window.open(book.pdfLink, "_blank");
